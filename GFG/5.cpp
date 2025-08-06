eacd57b197d1be6296c5929ec181f120187da908
Niteshsaini <itsniteshsaini6376@gmail.com>
2025-08-06 18:06:48 +0530

    number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold

--- GFG/5.cpp
+++ GFG/5.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-    int arr[] = {11, 13, 17, 23, 29, 31, 7, 5, 2, 3};
+    int arr[] = {2,2,2,2,5,5,5,8};
     int k = 3; 
     int n = sizeof(arr) / sizeof(arr[0]); 
-    int thr = 5;
+    int thr = 4;
     int count = 0;
     int sum = 0;
 
