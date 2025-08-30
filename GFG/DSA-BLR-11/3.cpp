f3532a595fba0413b77eedeeb437a3837264080d
Niteshsaini <itsniteshsaini6376@gmail.com>
2025-08-30 12:58:22 +0530

    DSA_BLR_11

--- GFG/DSA-BLR-11/3.cpp
+++ GFG/DSA-BLR-11/3.cpp
@@ -1,11 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+    int num = 24;
 
-int main(){
-    
+    int root = sqrt(num); 
 
-
+    for (int i = 1; i <= root; i++) {
+        if (num % i == 0) {
+            cout << i << " ";  
+            if (num / i != i) {
+                cout << num / i << " ";  
+            }
+        }
+    }
 
     return 0;
 }
