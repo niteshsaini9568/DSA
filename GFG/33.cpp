9cb8ab9d098fe976b10ffd1a445636df58e06815
Niteshsaini <itsniteshsaini6376@gmail.com>
2025-08-31 16:17:38 +0530

    DSA_BLR_11

---
+++ GFG/33.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+int main() {
+
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+
+    cout << "Original List: ";
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;34
+
+    Node* prev = NULL;
+    Node* curr = head;
+    while (curr != NULL) {
+        Node* nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+    head = prev;
+
+    cout << "Reversed List: ";
+    temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+
+    return 0;
+}
---
+++ GFG/34.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void binary_to_decimal(vector<int> arr){
+    int n = arr.size();
+    int p = 1;
+    int num = 0;
+
+    for(int i = n - 1; i >= 0; i--){
+        if(arr[i] == 1) num += p;
+        p *= 2;
+    };
+
+    cout << num;
+}
+
+// Binary to Decimal
+int main(){
+    vector<int> arr;
+    int x;
+
+    cout << "How many Binary Digits you have = ";
+    cin >> x;
+
+    while(x > 0){
+        int inp;
+        cin >> inp;
+        arr.push_back(inp);
+        x--;
+    }
+
+    binary_to_decimal(arr);
+    
+    return 0;
+}
