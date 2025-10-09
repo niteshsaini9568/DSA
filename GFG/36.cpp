84d437f5cb863125cb21cebb2ffd548054ec37d2
Niteshsaini <itsniteshsaini6376@gmail.com>
2025-10-09 15:44:51 +0530

    preorder

---
+++ GFG/36.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    } 
+};
+
+void preorder(Node* node){
+    if(node == NULL){
+        return;
+    }
+    cout << node->data <<endl;
+    preorder(node->left);
+    preorder(node->right);
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    preorder(root);
+    return 0;
+}
