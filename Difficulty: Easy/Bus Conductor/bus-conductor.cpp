6278897ac0a987f2e8e4166e890eeefada7a9c3c
Nitesh saini <itsniteshsaini6376@gmail.com>
2025-12-19 23:06:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bus Conductor/bus-conductor.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int findMoves(vector<int>& chairs, vector<int>& passengers) {
+        // code here
+        sort(chairs.begin(), chairs.end());
+        sort(passengers.begin(), passengers.end());
+        
+        int count = 0;
+        int n = chairs.size();
+        
+        for(int i = 0; i < n; i++){
+            int diff = abs(chairs[i] - passengers[i]);
+            count += diff;
+        }
+        
+        return count;
+    }
+};
