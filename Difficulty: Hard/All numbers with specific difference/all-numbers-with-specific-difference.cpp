545cb4c89be3f854e90ca5e1c1bb269e19236dc9
Nitesh saini <itsniteshsaini6376@gmail.com>
2026-02-13 12:21:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/All numbers with specific difference/all-numbers-with-specific-difference.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    bool isGreater(int val, int d) {
+    	int digitSum = 0, tmp = val;
+    	while (tmp>0) {
+    		digitSum += tmp%10;
+    		tmp /= 10;
+    	}
+    
+    	return val - digitSum >= d;
+    }
+    
+    int getCount(int n, int d) {
+    	
+    	// Minimum number for which difference between 
+    	// number and sum of digits >= d.
+    	int mini = n + 1;
+    	int s = 1, e = n;
+    	
+    	while (s<=e) {
+    		int mid = s + (e-s)/2;
+            
+    		if (isGreater(mid, d)) {
+    			mini = mid;
+    			e = mid - 1;
+    		}
+    		else s = mid + 1;
+    	}
+        
+
+    	return n+1-mini;
+    }
+};
