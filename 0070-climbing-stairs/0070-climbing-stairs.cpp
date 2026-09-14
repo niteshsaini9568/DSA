24c360616aabd43a528106013f6a72c4f1040e5d
Nitesh saini <itsniteshsaini6376@gmail.com>
2026-09-14 10:58:53 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int dp[46];
-    int solve(int n){
-        if(n < 0) return 0;
-        if(n == 0) return 1;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = solve(n-1) + solve(n-2);
-    }
-
-
     int climbStairs(int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(n);
+        if(n == 0 || n == 1 || n == 2) return n;
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        for(int i = 3; i <= n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
     }
 };
