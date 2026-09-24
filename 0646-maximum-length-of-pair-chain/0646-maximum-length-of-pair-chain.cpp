9c5d9c799d3e1f7b0bf902b766719b2163e7fa34
Nitesh saini <itsniteshsaini6376@gmail.com>
2026-09-24 11:32:14 +0530

    Time: 5 ms (70.9%), Space: 26.4 MB (93.44%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
+    // int findLongestChain(vector<vector<int>>& pairs) {
+    //     sort(pairs.begin(), pairs.end());
+    //     int n = pairs.size();
+    //     vector<int> dp(n, 1);
+    //     int ans = 1;
+
+    //     for(int i = 1; i < n; i++){
+    //         for(int j = 0; j < i; j++){
+    //             if(pairs[j][1] < pairs[i][0]){
+    //                 dp[i] = max(dp[i], dp[j] + 1);
+    //             }
+    //         }
+    //         ans = max(ans, dp[i]);
+    //     }
+
+    //     return ans;
+    // }
     int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(), pairs.end());
-        int n = pairs.size();
-        vector<int> dp(n, 1);
-        int ans = 1;
+        sort(pairs.begin(), pairs.end(), [](const vector<int>& a, const vector<int>& b){
+            return a[1] < b[1];
+        });
 
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(pairs[j][1] < pairs[i][0]){
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
-            }
-            ans = max(ans, dp[i]);
-        }
+        int count = 0, chainEnd = INT_MIN;
+        for (const auto& pair : pairs)
+            if (pair[0] > chainEnd) { count++; chainEnd = pair[1]; }
 
-        return ans;
+        return count;
     }
+
 };
